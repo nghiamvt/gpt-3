e82d63518c216a200dd0fbd443a134ed9ceec121
Nghia Mai <nghia.mai@foodpanda.com>
2022-05-13 01:46:37 -0400

    add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Button from '@mui/material/Button';
+
 function App() {
   return (
     <div className="App">
       <h1>hi</h1>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </div>
   );
 }
