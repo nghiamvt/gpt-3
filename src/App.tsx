9f01009583ef7092a9408076644fc78af1ddf74e
Nghia Mai <nghia.mai@foodpanda.com>
2022-05-14 11:33:45 -0400

    bugfix style

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,20 @@ import SideBar from './SideBar';
 function App() {
   return (
     <Box sx={{ bgColor: "#F4F5FA" }}>
-      <Container sx={{ py: { xs: 0, md: 10 } }}>
+      <Container>
         <Paper
           sx={{
             display: "flex",
             flexDirection: "column",
-            height: { xs: "95vh", md: "50rem" },
+            height: "calc(100vh - 2rem)",
+            m: 2,
           }}
         >
           <Grid container direction="row" height="100%">
             <Grid item md={4} display={{ xs: "none", md: "flex" }}>
               <SideBar />
             </Grid>
-            <Grid item md={8} height="100%">
+            <Grid item md={8} xs={12} height="100%">
               <Chat />
             </Grid>
           </Grid>
